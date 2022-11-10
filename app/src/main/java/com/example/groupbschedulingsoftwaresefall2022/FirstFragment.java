b297d6f155ac44add21afc75ba7f3851c8d914b0
Asir Nayeef <an739043@sju.edu>
2022-11-10 11:43:58 -0500

    Tried getting createschedButton to go to ScheduleNamePopup.

--- app/src/main/java/com/example/groupbschedulingsoftwaresefall2022/FirstFragment.java
+++ app/src/main/java/com/example/groupbschedulingsoftwaresefall2022/FirstFragment.java
@@ -29,11 +29,12 @@ public class FirstFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        binding.buttonFirst.setOnClickListener(new View.OnClickListener() {
+
+        binding.createschedButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 NavHostFragment.findNavController(FirstFragment.this)
-                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
+                    .navigate(R.id.action_SecondFragment_to_FirstFragment);
             }
         });
     }
