aec829c53e9f5a1d16fcddc51731c79f74b8e86d
SankeerthShabad <ss760792@sju.edu>
2022-12-02 21:17:26 -0500

    worked on CalendarView.java to create an event and store in database

--- app/src/main/java/com/example/groupbschedulingsoftwaresefall2022/CalendarView.java
+++ app/src/main/java/com/example/groupbschedulingsoftwaresefall2022/CalendarView.java
@@ -1,45 +1,68 @@
 package com.example.groupbschedulingsoftwaresefall2022;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-
+import android.widget.EditText;
 import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-import androidx.navigation.fragment.NavHostFragment;
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.CalendarView;
 
+public class MyCalendarView extends AppCompatActivity {
 
-import com.example.groupbschedulingsoftwaresefall2022.databinding.ViewCalendarBinding;
+    private mySQLiteDBHandler dbHandler;
+    private EditText editText;
+    private CalendarView calendarView;
+    private String selectedDate;
+    private SQLiteDatabase sqLiteDatabase;
 
-
-public class CalendarView extends Fragment {
-    private ViewCalendarBinding binding;
     @Override
-    public View onCreateView(
-            LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState
-    ) {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.calendar_view);
+        editText = findViewById(R.id.editTextTextPersonName);
+        calendarView = findViewById(R.id.calendarViewId);
 
-        binding = ViewCalendarBinding.inflate(inflater, container, false);
-        return binding.getRoot();
-
-    }
-    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-
-        binding.forwardButton.setOnClickListener(new View.OnClickListener() {
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
-            public void onClick(View view) {
-                NavHostFragment.findNavController(CalendarView.this)
-                        .navigate(R.id.action_calendarView2_to_FirstFragment);
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                selectedDate = Integer.toString(year) + month + dayOfMonth;
+                System.out.print("selectedDate: "+selectedDate);
             }
         });
+
+        try{
+
+            dbHandler = new mySQLiteDBHandler(this, "CalendarDatabase", null,1);
+            sqLiteDatabase = dbHandler.getWritableDatabase();
+            sqLiteDatabase.execSQL("CREATE TABLE EventCalendar(Date TEXT, Event TEXT)");
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
     }
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
+
+    public void InsertDatabase(View view){
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("Date",selectedDate);
+        contentValues.put("Event", editText.getText().toString());
+        sqLiteDatabase.insert("EventCalendar", null, contentValues);
+
     }
+
+    public void ReadDatabase(View view){
+        String query = "Select Event from EventCalendar where Date = " + selectedDate;
+        try{
+            Cursor cursor = sqLiteDatabase.rawQuery(query, null);
+            cursor.moveToFirst();
+            editText.setText(cursor.getString(0));
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            editText.setText("");
+        }
+    }
+
 }
